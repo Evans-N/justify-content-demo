2e9bd29b50dda81c5aa0652b7270e69722ae9f18
Nathan Evans <nathanevans777@gmail.com>
2019-07-12 10:51:43 -0400

    First Javascript interaction

---
+++ script/index.js
@@ -0,0 +1,6 @@
+const flexStart = document.querySelector('[data-flex-start]');
+const flexStartDiv = document.querySelector('[data-flex-start-div]');
+
+flexStart.addEventListener('click', () => {
+    flexStartDiv.classList.toggle('hidden');
+});
